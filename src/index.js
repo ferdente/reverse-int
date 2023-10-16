d48f260d35cd1d70b0a818f14924ef15191a083d
ferdente <64047548+ferdente@users.noreply.github.com>
2023-10-16 13:23:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+        if (n == 0) {
+            return 0;
+        } else {
+            let reverse = 0;
+            n = Math.abs(n)
+                while (n != 0) {
+                    reverse = reverse * 10 + n % 10;
+                    n = Math.floor(n / 10);
+                }
+        return reverse;
+    }
 }
